74646e55f7db5d4b955926e949c24aaf5e103471
SorocanAlexandru <sorocan2@gmail.com>
2022-11-08 20:04:00 +0300

    fg

--- Models/Book.cs
+++ Models/Book.cs
@@ -22,6 +22,11 @@ namespace Sorocan_Alexandru_Lab2.Models
         public int? PublisherID { get; set; }
         public Publisher? Publisher { get; set; }
 
+        public int? CategoryID { get; set; }
+        public Category? Category { get; set; }
+
+
+
         public ICollection<BookCategory>? BookCategories { get; set; }
 
 
